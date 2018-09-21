cc6a773bdaf5fd0e92bb6bf3afb336d21419a0f5
Osman Musse <hanad@portableuniverses-iMac.local>
2018-09-21 15:41:03 +0100

    pushing all files

--- app/js/app.js
+++ app/js/app.js
@@ -2764,4 +2764,16 @@ owlPrev.style.color = 'transparent';
 
 let owlNext = document.querySelector('.owl-next span');
 owlNext.style.color = 'transparent';
+
+let grid = document.querySelectorAll(".grid-container .grid");
+
+for (let i = 0; i < grid.length; i++) {
+  grid[i].addEventListener('mouseover', () => {
+    grid[i].classList.add("animated", "bounce");
+  });
+
+  grid[i].addEventListener('mouseout', () => {
+    grid[i].classList.remove("animated", "bounce");
+  });
+}
 //# sourceMappingURL=app.js.map
